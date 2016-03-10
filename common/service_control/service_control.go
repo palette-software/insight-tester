5062d9c51445cc45c831ec2dd59364ed76be3ea0
Peter Bastyi <bastyip@starschema.net>
2016-03-10 14:38:52 +0100

    Service control interface is not necessary

--- common/service_control/service_control.go
+++
@@ -1,8 +0,0 @@
-package service_control
-
-type ServiceControl interface {
-	Install(svcName, svcDescription string) error
-	Remove(svcName string) error
-	Start(svcName string) error
-	Stop(svcName string) error
-}
--- common/service_control/service_control_windows.go
+++ common/service_control/service_control_windows.go
@@ -4,22 +4,22 @@ import (
 	"golang.org/x/sys/windows/svc"
 )
 
-type ServiceControlWindows struct {
+type ServiceControl struct {
 
 }
 
-func (sc *ServiceControlWindows) Install(svcName, svcDescription string) error {
+func (sc *ServiceControl) Install(svcName, svcDescription string) error {
 	return installService(svcName, svcDescription)
 }
 
-func (sc *ServiceControlWindows) Remove(svcName string) error {
+func (sc *ServiceControl) Remove(svcName string) error {
 	return removeService(svcName)
 }
 
-func (sc *ServiceControlWindows) Start(svcName string) error {
+func (sc *ServiceControl) Start(svcName string) error {
 	return startService(svcName)
 }
 
-func (sc *ServiceControlWindows) Stop(svcName string) error {
+func (sc *ServiceControl) Stop(svcName string) error {
 	return controlService(svcName, svc.Stop, svc.Stopped)
 }
