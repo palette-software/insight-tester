e97a0e9e38351295df0e746ad0a4e6303d4e256c
szilardhuber <szilard.huber@gmail.com>
2016-05-23 07:42:03 +0200

    Update dbcheck to new config.

--- dbcheck/config.go
+++ dbcheck/config.go
@@ -1,40 +1,48 @@
 package main
 
 import (
-    "os"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
 )
 
 type Database struct {
-    Host string `yaml:"Host"`
-    Port int `yaml:"Port"`
-    Database string `yaml:"Database"`
-    User string `yaml:"User"`
-    Password string `yaml:"Password"`
+	Host     string `yaml:"Host"`
+	Port     int    `yaml:"Port"`
+	Database string `yaml:"Database"`
+	User     string `yaml:"User"`
+	Password string `yaml:"Password"`
 }
 
 type Config struct {
-    Database Database `yaml:"Database"`
+	Database Database `yaml:"Database"`
 }
 
-func getResultDBConfig(fileName string) (Database) {
-    var v Config
-    input, err := os.Open(fileName)
-    if err != nil {
-        Error.Println("Error opening file: ", err)
-        os.Exit(1)
-    }
-    defer input.Close()
-    b, err := ioutil.ReadAll(input)
-    if err != nil {
-        Error.Println("Error reading file: ", err)
-        os.Exit(1)
-    }
-     err = yaml.Unmarshal(b, &v)
-     if err != nil {
-        Error.Println("Error parsing xml", err)
-        os.Exit(1)
-    }
-    return v.Database
+func getResultDBConfig(fileName string) Database {
+	var v Config
+	input, err := os.Open(fileName)
+	if err != nil {
+		Error.Println("Error opening file: ", err)
+		os.Exit(1)
+	}
+	defer input.Close()
+	b, err := ioutil.ReadAll(input)
+	if err != nil {
+		Error.Println("Error reading file: ", err)
+		os.Exit(1)
+	}
+	err = yaml.Unmarshal(b, &v)
+	if err != nil {
+		Error.Println("Error parsing yml", err)
+		os.Exit(1)
+	}
+	if v.Database.Host == "" ||
+		v.Database.Port == 0 ||
+		v.Database.User == "" ||
+		v.Database.Database == "" ||
+		v.Database.Password == "" {
+		Error.Println("Config file does not contain database information.")
+		os.Exit(1)
+	}
+	return v.Database
 }
--- dbcheck/db.go
+++ dbcheck/db.go
@@ -1,51 +1,66 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	_ "github.com/lib/pq"
 )
 
-func getConnectionString(config Database) (string) {
-    return fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable", config.User, config.Password, config.Database, config.Host, config.Port)
+func getConnectionString(config Database) string {
+	return fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable", config.User, config.Password, config.Database, config.Host, config.Port)
 }
 
 var DBConnection *sql.DB
 
-func getConnection(database Database) (*sql.DB) {
-    if DBConnection == nil {
-        connectionString := getConnectionString(database)
-        var err error
-        DBConnection, err = sql.Open("postgres", connectionString)
-        if err != nil {
-            Error.Println("Error connecting to DB:", err)
-            return nil
-        }
-    }
-    return DBConnection
+func getConnection(database Database) *sql.DB {
+	if DBConnection == nil {
+		connectionString := getConnectionString(database)
+		var err error
+		DBConnection, err = sql.Open("postgres", connectionString)
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Error("Error connecting to DB:")
+			return nil
+		}
+	}
+	return DBConnection
 }
 
 func closeDB() {
-    DBConnection.Close()
+	DBConnection.Close()
 }
 
-func check(database Database, test Test) (bool) {
-    db := getConnection(database)
-    if db == nil {
-        return false
-    }
-    rows, err := db.Query(test.Sql)
-    if err != nil {
-        Error.Println("Error getting rows:", err)
-        return false
-    }
-    defer rows.Close()
-    for rows.Next() {
-        var count int
-        rows.Scan(&count)
-        if !checkTest(count, test) {
-            return false
-        }
-    }
-    return true
+func check(database Database, test Test) bool {
+	db := getConnection(database)
+	if db == nil {
+		return false
+	}
+	rows, err := db.Query(test.Sql)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Error getting rows:")
+		return false
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var count int
+		rows.Scan(&count)
+		if !checkTest(count, test) {
+			logrus.WithFields(logrus.Fields{
+				"description": test.Description,
+				"expected":    fmt.Sprintf("%s%d", test.Result.Operation, test.Result.Count),
+				"actual":      count,
+			}).Error("Failed DB sanity check.")
+			return false
+		}
+		logrus.WithFields(logrus.Fields{
+			"description": test.Description,
+			"expected":    fmt.Sprintf("%s%d", test.Result.Operation, test.Result.Count),
+			"actual":      count,
+		}).Info("Successful test")
+	}
+	return true
 }
