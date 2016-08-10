46f76917bbaf0c1cfc778a25b81b9c88b0da5dee
Peter Bastyi <baspeti@gmail.com>
2016-08-11 00:30:51 +0200

    Code cleanup

--- github-release-upload.py
+++ github-release-upload.py
@@ -1,4 +1,3 @@
-#import httplib, urllib
 import os
 import sys
 import requests
@@ -10,36 +9,27 @@ def getRequiredEnvVar(name):
         sys.exit(1)
     return envVar
 
+# Creates a new Github release and returns the ID of the newly created release. Although it returns the existing
+# release ID, if the release already exits.
 def main():
     owner = getRequiredEnvVar('OWNER')
     package = getRequiredEnvVar('PACKAGE')
     productVersion = getRequiredEnvVar('PRODUCT_VERSION')
     githubToken = getRequiredEnvVar('GITHUB_TOKEN')
 
-    # conn = httplib.HTTPSConnection("api.github.com")
-
     githubApiBaseUrl = 'https://api.github.com'
-    #payloadCreateRelease = "{\"tag_name\": \"v1.0.15\"}"
-    # urllib.urlencode({'tag_name': 'v1.0.15'})
-    # conn.set_debuglevel(1)
-    # conn.request("POST", "/repos/palette-software/insight-tester/releases", payloadCreateRelease, headers)
-    # r1 = conn.getresponse()
-    #
-    # print r1.status, r1.reason
-
     headers = {'Authorization': 'token ' + githubToken}
-
     payload = {'tag_name': productVersion}
 
-
     r = requests.post(githubApiBaseUrl + '/repos/' + owner + '/' + package + '/releases', json=payload, headers=headers)
-    print r.status_code, r.reason
-    print r.text
+    # print r.status_code, r.reason
+    # print r.text
     responseJson = r.json()
     if r.status_code == 200:
         releaseId = responseJson['id']
         if releaseId is None:
             # This is unexpected. There is supposed to be a Github release ID in the response JSON.
+            print 'Error! Release ID was not found in JSON response!'
             sys.exit(1)
 
         # We are all good, let's print the ID of the newly created Github release
@@ -49,25 +39,22 @@ def main():
     releaseAlreadyExists = False
 
     if r.status_code == 422:
-        print responseJson['message']
         for error in responseJson['errors']:
-            print error['code']
             if error['code'] == "already_exists":
                 releaseAlreadyExists = True
                 break
         if releaseAlreadyExists == False:
+            print 'Error! Release was expected to already exist, but it does not.'
             sys.exit(2)
 
         r = requests.get(githubApiBaseUrl + '/repos/' + owner + '/' + package + '/releases', headers=headers)
-        print 'Listing existing releases'
-        print r.status_code, r.reason
-        # print r.text
 
         for ver in r.json():
             if ver['tag_name'] == productVersion:
                 releaseId = ver['id']
                 if releaseId is None:
                     # This is unexpected. There is supposed to be a Github release ID in the response JSON.
+                    print 'Error! Release ID was not found in the existing Github release!'
                     sys.exit(1)
 
                 # We are all good, let's print the ID of the existing Github release
