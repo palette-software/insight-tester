7718b47ed9f4df143744aaf00b3e5e2fc2f38c28
Peter Bastyi <baspeti@gmail.com>
2016-08-11 00:36:43 +0200

    Fix encrypted Github token and use python Github release ID handler script on deploy

--- release-to-github.sh
+++ release-to-github.sh
@@ -10,17 +10,18 @@ if [ "X" == "X$HOME" ]; then echo "HOME environment variable is not set!"; exit 
 # if [ $? -ne 0 ]; then echo "uploading new version failed"; exit 10; fi
 
 echo "Creating Github realase..."
-GITHUB_RESPONSE=`curl -H "Authorization: token $GITHUB_TOKEN" -d "{\"tag_name\": \"$PRODUCT_VERSION\"}" "https://api.github.com/repos/$OWNER/$PACKAGE/releases"`
-# Parse the release ID from the the GitHub response.
-# NOTE: This is an ugly workaround, because we didn't manage to get jsawk working on Travis machines.
-echo "GitHub response: $GITHUB_RESPONSE"
-echo `grep --version` 
-ID_LINE=`echo "${GITHUB_RESPONSE}" | grep '^\s\s"id":\s\d*'` 
-echo "ID_LINE=$ID_LINE"
-ID_STRING=`echo "${ID_LINE}" | cut -d' ' -f 4`
-echo "ID_STRING=$ID_STRING"
-RELEASE_ID=`echo $ID_STRING | cut -d',' -f 1`
-echo "RELEASE_ID=$RELEASE_ID"
+# GITHUB_RESPONSE=`curl -H "Authorization: token $GITHUB_TOKEN" -d "{\"tag_name\": \"$PRODUCT_VERSION\"}" "https://api.github.com/repos/$OWNER/$PACKAGE/releases"`
+# # Parse the release ID from the the GitHub response.
+# # NOTE: This is an ugly workaround, because we didn't manage to get jsawk working on Travis machines.
+# echo "GitHub response: $GITHUB_RESPONSE"
+# echo `grep --version` 
+# ID_LINE=`echo "${GITHUB_RESPONSE}" | grep '^\s\s"id":\s\d*'` 
+# echo "ID_LINE=$ID_LINE"
+# ID_STRING=`echo "${ID_LINE}" | cut -d' ' -f 4`
+# echo "ID_STRING=$ID_STRING"
+# RELEASE_ID=`echo $ID_STRING | cut -d',' -f 1`
+# echo "RELEASE_ID=$RELEASE_ID"
+RELEASE_ID=`python github-release-upload.py`
 if [ $? -ne 0 ]; then echo "Creating new release failed"; exit 10; fi
 echo $RELEASE_ID
 if [ "X" == "X$RELEASE_ID" ]; then echo "Release ID was not found in GitHub response"; exit 10; fi
